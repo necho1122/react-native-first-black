969b81a5d940950c27c6848fe61f8ed31e956903
necho1122 <nelsonubac90@gmail.com>
2024-10-13 20:41:31 -0300

    react native

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Main from './Components/main';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaProvider>
+			<View style={styles.container}>
+				<StatusBar style='auto' />
+				<Main />
+			</View>
+		</SafeAreaProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
---
+++ Components/main.jsx
@@ -0,0 +1,84 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {
+	View,
+	Text,
+	ScrollView,
+	StyleSheet,
+	Image,
+	ActivityIndicator,
+} from 'react-native';
+import { moviesArray } from '../lib/moviesData';
+import { StatusBar } from 'expo-status-bar';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+function Main() {
+	const insets = useSafeAreaInsets(); // Obtener los insets
+
+	return (
+		<ScrollView
+			style={{
+				flex: 1,
+				padding: 20,
+				backgroundColor: '#fff',
+				marginTop: insets.top,
+				marginBottom: insets.bottom,
+			}}
+		>
+			<StatusBar style='auto' />
+			<Text style={styles.header}>Popular Movies</Text>
+			{moviesArray.map((movie) => (
+				<View
+					key={movie.id}
+					style={styles.movieItem}
+				>
+					<Text style={styles.title}>{movie.title}</Text>
+					<Image
+						source={{ uri: movie.poster }}
+						style={styles.poster}
+					/>
+					<Text style={styles.overview}>{movie.overview}</Text>
+					<Text style={styles.rating}>Rating: {movie.rating}</Text>
+				</View>
+			))}
+		</ScrollView>
+	);
+}
+
+const styles = StyleSheet.create({
+	header: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		marginBottom: 20,
+		textAlign: 'center',
+	},
+	movieItem: {
+		marginBottom: 20,
+		padding: 10,
+		borderRadius: 8,
+		backgroundColor: '#f0f0f0',
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		marginBottom: 10,
+	},
+	poster: {
+		width: '100%',
+		height: 300,
+		borderRadius: 8,
+		marginBottom: 10,
+	},
+	overview: {
+		fontSize: 16,
+		color: '#555',
+		marginBottom: 10,
+	},
+	rating: {
+		fontSize: 16,
+		fontWeight: 'bold',
+		color: '#333',
+	},
+});
+
+export default Main;
